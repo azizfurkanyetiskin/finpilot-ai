17bff2cb2798397bb857bdd2ac85e384c827773e
Furkan <azizfurkanyetiskin@gmail.com>
2026-07-23 15:28:33 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/azizfurkan/finpilotai/FinpilotAiApplication.java
@@ -0,0 +1,13 @@
+package com.azizfurkan.finpilotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpilotAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinpilotAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azizfurkan/finpilotai/FinpilotAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizfurkan.finpilotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinpilotAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
